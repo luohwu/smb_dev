7c70b580c768a756a52fbf2f3b4d352562e8dfdc
mantelt <thomas.mantel@mavt.ethz.ch>
2021-05-11 08:32:47 +0200

    Update setup_hw.sh
    * install librealsense2-dev
    * adding ddynamic-reconfigure as dependency of realsense2_camera

--- setup_hw.sh
+++ setup_hw.sh
@@ -6,7 +6,10 @@ read -p "Installing necessary libraries for RealSense. Press enter to continue..
 sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
 sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main"
 sudo apt update
-sudo apt-get install librealsense2-dkms librealsense2-utils
+sudo apt install librealsense2-dev librealsense2-dkms librealsense2-utils
+
+# Dependency of realsense2_camera
+sudo apt install ros-noetic-ddynamic-reconfigure
 
 # Necessary libraries for RoboSense driver
 read -p "Installing necessary libraries for RoboSense. Press enter to continue..."
